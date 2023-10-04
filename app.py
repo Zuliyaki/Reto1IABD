0da1fd56fd0a327030f8386021a581a0db5995d2
Unai Bonilla Hernández <unaibh01@gmail.com>
2023-10-04 21:06:49 +0200

    Acceso a base de datos

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def index():
 def principal(user):
     user_title = user.title()
     cursor = con.cursor()
-    cursor.execute("SELECT * FROM cine.peliculas")
+    cursor.execute("SELECT * FROM cine.peliculas order by movieid")
     data = cursor.fetchall()
     peliculas = []
     for i in range(0, len(data)):
@@ -29,11 +29,22 @@ def principal(user):
 # Abrir pagina web
 @app.route('/home/<user>/<pelicula>')
 def webPeli(user, pelicula):
-    user_title = user.title()
+    user_name = user.title()
     archivo = pelicula
     palabras = pelicula.split("-")  # Dividir la cadena en palabras usando "-" como separador
     texto_formateado = " ".join(palabra.lower().title() for palabra in palabras)
-    return render_template('pelicula.html', contenido = texto_formateado, archivo = archivo, usuario = user_title)
+    cursor = con.cursor()
+    cursor.execute("SELECT movieid, calificacionpromedio, reproducciones, sinopsis FROM cine.peliculas where titulo = %s", (texto_formateado,))
+    data = cursor.fetchall()
+    infoPeli = {
+        'id': data[0][0],
+        'titulo': texto_formateado,
+        'valoracion': data[0][1],
+        'reproducciones': data[0][2],
+        'sinopsis': data[0][3],
+        'codaName': archivo
+    }
+    return render_template('pelicula.html', usuario = user_name, datos = infoPeli)
 
 # Pedir pelicula
 @app.route('/getPeli', methods = ['POST'])
@@ -43,6 +54,22 @@ def getPeli():
     resultado = con.getPeli(con, id)
     print(resultado)
     return jsonify(resultado)
+
+@app.route('/actualizarReproducciones', methods=['PUT'])
+def actualizar_reproducciones():
+    try:
+        cursor = con.cursor()
+        cursor.execute("SELECT reproducciones FROM cine.peliculas WHERE id = 1")
+        valor_actual = cursor.fetchone()[0]
+        nuevo_valor = valor_actual + 1
+        cursor.execute("UPDATE cine.peliculas SET valor = %s WHERE id = 1", (nuevo_valor,))
+        con.commit()
+        cursor.close()
+        return str(nuevo_valor)
+    except Exception as e:
+        # Manejo de errores
+        con.rollback()
+        return 'Error en la actualización: ' + str(e)
 
 """ METODO AÑADIR A LA BASE DE DATOS
 @app.route('/insertar', methods=['POST'])
